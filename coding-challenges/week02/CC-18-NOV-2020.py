43153ac24becbea3d6245948d5d2d2798b9c2eb2
Akash Verma <akashcool2014.av@gmail.com>
2020-11-18 11:49:03 +0530

    added coding challenge for 18-nov-2020

---
+++ coding-challenges/week02/CC-18-NOV-2020.py
@@ -0,0 +1,20 @@
+print("This is a Simple Calcualtor")     # this was the assignment given to just print all arithematic operations
+
+print("Give me the First number")        # asking for the first number from user
+n1 = float(input())                      # taking float values so all type of numbers can be obtained 
+
+print("Give me the Second number")       # asking for the second number from user
+n2 = float(input())                       # taking float values so all type of numbers can be obtained 
+
+print("Addition of these will be : ", n1 + n2)         # performing Addition
+
+print("Subtraction of these will be : ", n1 - n2)      # performing Subtraction
+
+print("Multiplication of these will be : ", n1 * n2)   # performing Multiplication
+ 
+print("Division of these will be : ", n1 / n2)         # performing Division 
+#                                                      # (here float values will print complete division)
+#                                                      # (otherwise the remainder in some case will be left)
+
+print("Thanks for your Input!! Press enter to exit!!")
+input()
