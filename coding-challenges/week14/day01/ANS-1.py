15b9ad111da5262af79de20cdda4f2136a14d631
Akash Verma <akashcool2014.av@gmail.com>
2021-02-09 23:40:47 +0530

    added CC and assignment

---
+++ coding-challenges/week14/day01/ANS-1.py
@@ -0,0 +1,30 @@
+"""
+Tiny URL:
+        A free URL shortening service that produces short, memorable alias URLs for
+        existing web pages. It enables users to enter a long URL and add a custom parameter
+        to create the shortened URL. This URL then redirects to the web page from the
+        original URL. The shortened URL never expires, so as long as the destination URL is
+        the same, the shortened version will always be functional.
+
+        These URLs are universally unique. These kinds of URLs are most commonly used in
+        email campaigns, social media posts, print advertisements, and other marketing efforts
+        where an easy-to-remember URL (or one with limited characters) is beneficial for
+        driving traffic to a specific page. It’s also helpful for adding UTM parameters to a link
+        without making them obvious to the audience.
+
+        Few things that we should ask them is:
+
+        1. MVP (Minimal Viable Project)
+        2. Calculations
+        3. High Level Picture
+
+        They basically deal with issues like:
+         Portability
+         Security
+         Maintainability
+         Reliability
+         Scalability
+         Performance
+         Reusability
+         Flexibility
+"""
