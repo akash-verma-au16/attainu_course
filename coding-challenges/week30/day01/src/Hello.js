87490dc66441536c58aa99b78046cbb140732dab
Akash Verma <akashcool2014.av@gmail.com>
2021-06-09 17:36:50 +0530

    added cc

---
+++ coding-challenges/week30/day01/src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello (from Functional Component)</h1>
+    </div>
+  );
+}
+
+export default Hello;
---
+++ coding-challenges/week30/day01/src/World.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class World extends Component {
+  render() {
+    return (
+      <div>
+        <h1>World (from class Component)</h1>
+      </div>
+    );
+  }
+}
+
+export default World;
